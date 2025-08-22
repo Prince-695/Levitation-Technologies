caac62636dc3b92100ca6a93c6c7949804544638
Prince Rathod <rathodprince411@gmail.com>
2025-08-22 11:21:31 +0530

    updates

--- server/app.js
+++ server/app.js
@@ -25,9 +25,9 @@ app.use(rateLimit({
 }));
 app.use(helmet());
 
-// CORS configuration
+// Environment-aware CORS configuration
 app.use(cors({
-  origin: process.env.CLIENT_URL || ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:5174'],
+  origin: process.env.CLIENT_URL ? process.env.CLIENT_URL.split(',') : ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:5174'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
