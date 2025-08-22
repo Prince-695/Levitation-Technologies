fec68f9b08891abab3d96878932617f31385f9b3
Prince Rathod <rathodprince411@gmail.com>
2025-08-22 13:16:30 +0530

    deployment updates

--- server/app.js
+++ server/app.js
@@ -27,7 +27,7 @@ app.use(helmet());
 
 // CORS configuration
 app.use(cors({
-  origin: process.env.CLIENT_URL || ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:5174'],
+  origin: process.env.CLIENT_URL,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
